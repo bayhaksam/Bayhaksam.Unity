f105dc6cbabb731a0c57692aa397b199ce7275ae
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-11-21 23:22:11 +0300

    Added behaviour for 'Start' Unity method

---
+++ src/src/src/src/Behaviour/OnStart.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="OnStart.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.Behaviour
+{
+	using UnityEngine;
+	using UnityEngine.Events;
+
+	public class OnStart : MonoBehaviour
+	{
+		#region Unity Events
+		public UnityEvent OnStartInvoked;
+		#endregion
+
+		#region Unity Methods
+		protected virtual void Start()
+		{
+			this.OnStartInvoked.Invoke();
+		}
+		#endregion
+	}
+}
