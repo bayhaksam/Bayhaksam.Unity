0a7d0fd7f49d2794d35a32b4104069be547479a1
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-11-21 23:31:33 +0300

    Added behaviour for 'OnDestroy' Unity method

---
+++ src/src/src/src/Behaviour/OnDestroyBehaviour.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="OnDestroyBehaviour.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.Behaviour
+{
+	using UnityEngine;
+	using UnityEngine.Events;
+
+	public class OnDestroyBehaviour : MonoBehaviour
+	{
+		#region Unity Events
+		public UnityEvent OnDestroyInvoked;
+		#endregion
+
+		#region Unity Methods
+		protected virtual void OnDestroy()
+		{
+			this.OnDestroyInvoked.Invoke();
+		}
+		#endregion
+	}
+}
