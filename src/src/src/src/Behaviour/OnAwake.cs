94ec6c4b0cc11f5486cd28249e0566418e403634
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-11-21 23:20:16 +0300

    Added behaviour for Awake Unity method

---
+++ src/src/src/src/Behaviour/OnAwake.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="OnAwake.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.Behaviour
+{
+	using UnityEngine;
+	using UnityEngine.Events;
+
+	public class OnAwake : MonoBehaviour
+	{
+		#region Unity Events
+		public UnityEvent OnAwakeInvoked;
+		#endregion
+
+		#region Unity Methods
+		protected virtual void Awake()
+		{
+			this.OnAwakeInvoked.Invoke();
+		}
+		#endregion
+	}
+}
