0119a61d5c8370e9b3cf52a9091aff9940059f9c
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-11-21 23:28:08 +0300

    Added behaviour for 'OnEnable' Unity method

---
+++ src/src/src/src/Behaviour/OnEnableBehaviour.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="OnEnableBehaviour.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.Behaviour
+{
+	using UnityEngine;
+	using UnityEngine.Events;
+
+	public class OnEnableBehaviour : MonoBehaviour
+	{
+		#region Unity Events
+		public UnityEvent OnEnableInvoked;
+		#endregion
+
+		#region Unity Methods
+		protected virtual void OnEnable()
+		{
+			this.OnEnableInvoked.Invoke();
+		}
+		#endregion
+	}
+}
