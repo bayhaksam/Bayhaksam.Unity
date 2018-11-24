a67ab8e712bce1a6fe26a796e3585f99d90012d7
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-11-24 23:57:04 +0300

    Added 'SetActiveToggler' behaviour

---
+++ src/src/src/src/Behaviour/SetActiveToggler.cs
@@ -0,0 +1,54 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="SetActiveToggler.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.Behaviour
+{
+	using UnityEngine;
+
+	public class SetActiveToggler : MonoBehaviour
+	{
+		#region Unity Fields
+		[SerializeField]
+		GameObject go;
+
+		[SerializeField]
+		bool isReversed;
+		#endregion
+
+		#region Properties
+		public bool IsReversed
+		{
+			get { return this.isReversed; }
+			set { this.isReversed = value; }
+		}
+
+		public GameObject GameObject
+		{
+			get
+			{
+				if (this.go != null)
+				{
+					return this.go;
+				}
+
+				return this.go = this.gameObject;
+			}
+			set
+			{
+				this.go = value;
+			}
+		}
+		#endregion
+
+		#region Public Methods
+		public void Execute(bool value)
+		{
+			this.GameObject.SetActive(!this.IsReversed ? value : !value);
+		}
+		#endregion
+	}
+}
