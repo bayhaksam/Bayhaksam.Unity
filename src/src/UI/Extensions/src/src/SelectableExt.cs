07b8cb12390f3c151b4dbfcfee62b93cc8b78c82
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-11-25 19:46:13 +0300

    Added 'Selectable' extensions

---
+++ src/src/UI/Extensions/src/src/SelectableExt.cs
@@ -0,0 +1,61 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="SelectableExt.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.UI.Extensions
+{
+	using UnityEngine;
+	using UnityEngine.UI;
+
+	/// <summary>
+	/// Extension class for <see cref="Selectable"/>.
+	/// </summary>
+	public static class SelectableExt
+	{
+		public static void SetColors(
+			this Selectable source,
+			Color disabledColor,
+			Color highlightedColor,
+			Color normalColor,
+			Color pressedColor)
+		{
+			var colors = source.colors;
+			colors.disabledColor = disabledColor;
+			colors.highlightedColor = highlightedColor;
+			colors.normalColor = normalColor;
+			colors.pressedColor = pressedColor;
+			source.colors = colors;
+		}
+
+		public static void SetDisabledColor(this Selectable source, Color color)
+		{
+			var colors = source.colors;
+			colors.disabledColor = color;
+			source.colors = colors;
+		}
+
+		public static void SetHighlightedColor(this Selectable source, Color color)
+		{
+			var colors = source.colors;
+			colors.highlightedColor = color;
+			source.colors = colors;
+		}
+
+		public static void SetNormalColor(this Selectable source, Color color)
+		{
+			var colors = source.colors;
+			colors.normalColor = color;
+			source.colors = colors;
+		}
+
+		public static void SetPressedColor(this Selectable source, Color color)
+		{
+			var colors = source.colors;
+			colors.pressedColor = color;
+			source.colors = colors;
+		}
+	}
+}
