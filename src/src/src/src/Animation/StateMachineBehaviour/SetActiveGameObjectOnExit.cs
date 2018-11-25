d692dd8e3959d4e5d66404d488b0e1186f08e323
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-11-26 01:19:21 +0300

    Added 'SetActiveGameObjectOnExit' animation state machine behaviour

---
+++ src/src/src/src/Animation/StateMachineBehaviour/SetActiveGameObjectOnExit.cs
@@ -0,0 +1,44 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="SetActiveGameObjectOnExit.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.Animation.StateMachineBehaviour
+{
+	using UnityEngine;
+
+	public class SetActiveGameObjectOnExit : StateMachineBehaviour
+	{
+		#region Unity Fields
+		[SerializeField]
+		bool isActivate = true;
+
+		[SerializeField]
+		GameObject target;
+		#endregion
+
+		#region Properties
+		public bool IsActivate
+		{
+			get { return this.isActivate; }
+			set { this.isActivate = value; }
+		}
+
+		public GameObject Target
+		{
+			get { return this.target; }
+			set { this.target = value; }
+		}
+		#endregion
+
+		#region Unity Methods
+		// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+		public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+		{
+			(this.Target != null ? this.Target : animator.gameObject).SetActive(this.IsActivate);
+		}
+		#endregion
+	}
+}
