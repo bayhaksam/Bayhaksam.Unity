f1874c452bae6b7b42f7aeeda70c4a94e38079ce
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-11-25 17:24:11 +0300

    Added animator player abstraction

---
+++ src/src/src/src/Animation/IAnimatorPlayer.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IAnimatorPlayer.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.Animation
+{
+	using UnityEngine;
+
+	public interface IAnimatorPlayer
+	{
+		#region Properties
+		Animator Animator { get; set; }
+		#endregion
+
+		#region Methods
+		void Play(string stateName);
+
+		void Play(string stateName, int layer);
+
+		void Play(string stateName, int layer, float normalizedTime);
+
+		void PlayInFixedTime(string stateName);
+
+		void PlayInFixedTime(string stateName, int layer);
+
+		void PlayInFixedTime(string stateName, int layer, float fixedTime);
+		#endregion
+	}
+}
