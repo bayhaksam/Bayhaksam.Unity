f23120701b42ec60240d88ce56650f0c7d5a5f03
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-11-25 19:59:02 +0300

    Added int unity event

---
+++ src/src/src/src/Events/IntUnityEvent.cs
@@ -0,0 +1,17 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IntUnityEvent.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.Events
+{
+	using System;
+	using UnityEngine.Events;
+
+	[Serializable]
+	public class IntUnityEvent : UnityEvent<int>
+	{
+	}
+}
