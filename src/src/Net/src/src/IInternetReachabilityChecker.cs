fe0bd4e4be7214d6b0cab3d80243e62c90a804d7
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-11-25 20:47:42 +0300

    Added internet reachability checker abstractions

---
+++ src/src/Net/src/src/IInternetReachabilityChecker.cs
@@ -0,0 +1,33 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IInternetReachabilityChecker.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.Net
+{
+	using Bayhaksam.Unity.Events;
+	using UnityEngine.Events;
+
+	public interface IInternetReachabilityChecker
+	{
+		#region Properties
+		bool AllowCarrierDataNetwork { get; set; }
+
+		bool AllowLocalAreaNetwork { get; set; }
+
+		string CheckYourInternetConnectionLabel { get; set; }
+		#endregion
+
+		#region Unity Events
+		UnityEvent OnInternetAvailable { get; set; }
+
+		StringUnityEvent OnInternetIsNotAvailable { get; set; }
+		#endregion
+
+		#region Methods
+		void Execute();
+		#endregion
+	}
+}
