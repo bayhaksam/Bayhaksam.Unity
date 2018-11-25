0a8030aa3c7740066d04893af782572b7d26a167
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-11-25 20:50:55 +0300

    Added internet reachability checker

---
+++ src/src/Net/src/src/Behaviour/InternetReachabilityChecker.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="InternetReachabilityChecker.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.Net.Behaviour
+{
+	public class InternetReachabilityChecker : InternetReachabilityCheckerBase
+	{
+		#region Public Methods
+		public override void Execute()
+		{
+			if (this.GetIsInternetAvailable())
+			{
+				this.OnInternetAvailable.Invoke();
+
+				return;
+			}
+
+			this.OnInternetIsNotAvailable.Invoke(this.CheckYourInternetConnectionLabel);
+		}
+		#endregion
+	}
+}
