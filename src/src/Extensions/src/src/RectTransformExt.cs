a2e82a689798e32f84cb3eba84400ed1a9066941
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-11-25 16:08:28 +0300

    Added 'RectTransform' extensions

---
+++ src/src/Extensions/src/src/RectTransformExt.cs
@@ -0,0 +1,31 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="RectTransformExt.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.Extensions
+{
+	using UnityEngine;
+
+	/// <summary>
+	/// <see cref="RectTransform"/>' s extension class
+	/// </summary>
+	public static class RectTransformExt
+	{
+		public static void SetRectTransform(this RectTransform source, RectTransform transform)
+		{
+			source.sizeDelta = transform.sizeDelta;
+			source.localScale = transform.localScale;
+			source.localPosition = transform.localPosition;
+			source.anchoredPosition = transform.anchoredPosition;
+			source.anchorMin = transform.anchorMin;
+			source.anchorMax = transform.anchorMax;
+			source.eulerAngles = transform.eulerAngles;
+			source.offsetMin = transform.offsetMin;
+			source.offsetMax = transform.offsetMax;
+			source.pivot = transform.pivot;
+		}
+	}
+}
