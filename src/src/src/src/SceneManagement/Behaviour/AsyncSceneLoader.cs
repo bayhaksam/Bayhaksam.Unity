5d3159e45390d8e7230d963762a891540c361d5a
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-11-27 09:22:58 +0300

    Added async scene loader implementation

---
+++ src/src/src/src/SceneManagement/Behaviour/AsyncSceneLoader.cs
@@ -0,0 +1,35 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AsyncSceneLoader.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.SceneManagement.Behaviour
+{
+	using UnityEngine;
+	using UnityEngine.SceneManagement;
+
+	public class AsyncSceneLoader : MonoBehaviour, ISceneLoader
+	{
+		public virtual void Load(int sceneBuildIndex)
+		{
+			SceneManager.LoadSceneAsync(sceneBuildIndex);
+		}
+
+		public virtual void Load(int sceneBuildIndex, LoadSceneMode mode)
+		{
+			SceneManager.LoadSceneAsync(sceneBuildIndex, mode);
+		}
+
+		public virtual void Load(string sceneName)
+		{
+			SceneManager.LoadSceneAsync(sceneName);
+		}
+
+		public virtual void Load(string sceneName, LoadSceneMode mode)
+		{
+			SceneManager.LoadSceneAsync(sceneName, mode);
+		}
+	}
+}
