8e1f33d0f97f30bc5d3e3f6557bb6e995ec07478
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-11-27 09:58:23 +0300

    Added player prefs scene loader implementations

---
+++ src/src/src/src/SceneManagement/Behaviour/playerPrefs/PlayerPrefsPreviousLoadedAsyncSceneLoader.cs
@@ -0,0 +1,51 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="PlayerPrefsPreviousLoadedAsyncSceneLoader.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.SceneManagement.Behaviour
+{
+	using System;
+	using UnityEngine;
+	using UnityEngine.SceneManagement;
+
+	public class PlayerPrefsPreviousLoadedAsyncSceneLoader
+		: PlayerPrefsPreviousLoadedSceneLoaderBase, IPreviousLoadedSceneAsyncLoader
+	{
+		#region Static Fields
+		/// <summary>
+		/// Stores value for lazy initialization.
+		/// </summary>
+		static Lazy<PlayerPrefsPreviousLoadedAsyncSceneLoader> lazyInstance;
+		#endregion
+
+		#region Static Properties
+		/// <summary>
+		/// Gets singleton instance.
+		/// </summary>
+		public static PlayerPrefsPreviousLoadedAsyncSceneLoader Instance => lazyInstance.Value;
+		#endregion
+
+		#region Unity Methods
+		protected virtual void Awake()
+		{
+			lazyInstance = new Lazy<PlayerPrefsPreviousLoadedAsyncSceneLoader>(
+				() => this.gameObject.GetComponent<PlayerPrefsPreviousLoadedAsyncSceneLoader>());
+		}
+		#endregion
+
+		#region Public Methods
+		public virtual AsyncOperation LoadAsync()
+		{
+			return SceneManager.LoadSceneAsync(this.GetPreviousSceneIndex());
+		}
+
+		public virtual AsyncOperation LoadAsync(LoadSceneMode mode)
+		{
+			return SceneManager.LoadSceneAsync(this.GetPreviousSceneIndex(), mode);
+		}
+		#endregion
+	}
+}
