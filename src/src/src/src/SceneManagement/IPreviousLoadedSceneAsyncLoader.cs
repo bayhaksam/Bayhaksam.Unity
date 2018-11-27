c6bc6b27989e8fd92ccfefb49b7fbb9c076dfe22
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-11-27 09:52:14 +0300

    Added previous scene loader abstractions

---
+++ src/src/src/src/SceneManagement/IPreviousLoadedSceneAsyncLoader.cs
@@ -0,0 +1,19 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IPreviousLoadedSceneAsyncLoader.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.SceneManagement
+{
+	using UnityEngine;
+	using UnityEngine.SceneManagement;
+
+	public interface IPreviousLoadedSceneAsyncLoader : IPreviousSceneLoader
+	{
+		AsyncOperation LoadAsync();
+
+		AsyncOperation LoadAsync(LoadSceneMode mode);
+	}
+}
