b7b84630e986fa3351885da30dec0e59f6e6236a
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-11-28 08:21:52 +0300

    Added 'OnDelayerdStart' behaviour

---
+++ src/src/src/src/Behaviour/OnDelayedStart.cs
@@ -0,0 +1,46 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="OnDelayedStart.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.Behaviour
+{
+	using System.Collections;
+	using UnityEngine;
+	using UnityEngine.Events;
+
+	public class OnDelayedStart : MonoBehaviour
+	{
+		#region Unity Fields
+		[SerializeField]
+		float seconds = 1.0f;
+
+		[SerializeField]
+		UnityEvent onDelayedStartInvoked;
+		#endregion
+
+		#region Unity Events
+		public UnityEvent OnDelayedStartInvoked
+		{
+			get => this.onDelayedStartInvoked;
+			set => this.onDelayedStartInvoked = value;
+		}
+		#endregion
+
+		#region Properties
+		public float Seconds { get => this.seconds; set => this.seconds = value; }
+		#endregion
+
+		#region Unity Methods
+		/// <inheritdoc/>
+		protected virtual IEnumerator Start()
+		{
+			yield return new WaitForSeconds(this.Seconds);
+
+			this.OnDelayedStartInvoked.Invoke();
+		}
+		#endregion
+	}
+}
