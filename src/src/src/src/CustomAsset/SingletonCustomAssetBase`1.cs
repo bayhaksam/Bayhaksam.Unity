00cda114a7f58f5260ee273d80bc20c74dc934bb
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-11-29 01:38:18 +0300

    Added singleton for scriptableobjects

---
+++ src/src/src/src/CustomAsset/SingletonCustomAssetBase`1.cs
@@ -0,0 +1,43 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="SingletonCustomAssetBase`1.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.CustomAsset
+{
+	using System;
+	using System.Linq;
+	using UnityEngine;
+
+	public abstract class SingletonCustomAssetBase<T> : ScriptableObject where T : SingletonCustomAssetBase<T>
+	{
+		#region Protected Constructors
+		protected SingletonCustomAssetBase()
+		{
+		}
+		#endregion
+
+		#region Static Properties
+		/// <summary>
+		/// Gets singleton instance.
+		/// </summary>
+		public static T Instance => LazyInstance.Value;
+		#endregion
+
+		#region Static Protected Properties
+		/// <summary>
+		/// Gets value for lazy initialization.
+		/// </summary>
+		protected static Lazy<T> LazyInstance { get; private set; }
+		#endregion
+
+		#region Unity Methods
+		protected virtual void Awake()
+		{
+			LazyInstance = new Lazy<T>(() => Resources.FindObjectsOfTypeAll<T>().FirstOrDefault());
+		}
+		#endregion
+	}
+}
