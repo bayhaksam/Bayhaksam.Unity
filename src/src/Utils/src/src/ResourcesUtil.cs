ea5837673a3547d37c0028cbfccf7243b00a3b6b
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-12-01 14:28:22 +0300

    Added utility class for 'Resources'

---
+++ src/src/Utils/src/src/ResourcesUtil.cs
@@ -0,0 +1,25 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ResourcesUtil.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.Utils
+{
+	using System.Linq;
+	using UnityEngine;
+
+	/// <summary>
+	/// Utility class for <see cref="Resources"/>.
+	/// </summary>
+	public class ResourcesUtil
+	{
+		#region Public Methods
+		public static Object FindObjectOfType(System.Type type)
+			=> Resources.FindObjectsOfTypeAll(type).FirstOrDefault();
+
+		public static T FindObjectOfType<T>() where T : Object => Resources.FindObjectsOfTypeAll<T>().FirstOrDefault();
+		#endregion
+	}
+}
