1eab8cfcae40dcd01a5c06e46136a1728510bae3
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-12-16 17:28:38 +0300

    Added abstractions for text printer

---
+++ src/src/UI/src/src/Behaviour/TextPrinterBase`1.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TextPrinterBase`1.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.UI.Behaviour
+{
+	using UnityEngine;
+	using UnityEngine.UI;
+
+	public abstract class TextPrinterBase<T> : MonoBehaviour, ITextPrinter<T>
+	{
+		#region Unity Fields
+		[SerializeField]
+		Text text;
+		#endregion
+
+		#region Properties
+		public virtual Text Text
+		{
+			get { return this.text; }
+			set { this.text = value; }
+		}
+		#endregion
+
+		#region Public Methods
+		public abstract void Execute(T value);
+		#endregion
+	}
+}
---
+++ src/src/UI/src/src/ITextPrinter`1.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ITextPrinter`1.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.UI
+{
+	using UnityEngine.UI;
+
+	public interface ITextPrinter<in T>
+	{
+		#region Properties
+		Text Text { get; set; }
+		#endregion
+
+		#region Methods
+		void Execute(T value);
+		#endregion
+	}
+}
