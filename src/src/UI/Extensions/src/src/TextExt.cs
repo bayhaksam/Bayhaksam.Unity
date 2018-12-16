32a92a71f069f37abef5198b275531dfd662f6ec
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-12-16 17:26:07 +0300

    Added 'SetText' extension method for Unity UI 'Text' element

---
+++ src/src/UI/Extensions/src/src/TextExt.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TextExt.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.UI.Extensions
+{
+	using UnityEngine.UI;
+
+	/// <summary>
+	/// Extensions for <see cref="Text"/> class.
+	/// </summary>
+	public static class TextExt
+	{
+		public static void SetText(this Text source, object value)
+		{
+			source.text = value.ToString();
+		}
+	}
+}
