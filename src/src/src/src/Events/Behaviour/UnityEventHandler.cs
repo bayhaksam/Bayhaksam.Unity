1367ef017994a2a498315766d6064de1d1f8fd56
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-12-20 23:13:40 +0300

    Added Unity event handler

---
+++ src/src/src/src/Events/Behaviour/UnityEventHandler.cs
@@ -0,0 +1,35 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="UnityEventHandler.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.Events.Behaviour
+{
+	using UnityEngine;
+	using UnityEngine.Events;
+
+	public class UnityEventHandler : MonoBehaviour
+	{
+		#region Unity Fields
+		[SerializeField]
+		UnityEvent onInvoked;
+		#endregion
+
+		#region Unity Events
+		public UnityEvent OnInvoked
+		{
+			get => this.onInvoked;
+			set => this.onInvoked = value;
+		}
+		#endregion
+
+		#region Public Methods
+		public virtual void Execute()
+		{
+			this.OnInvoked.Invoke();
+		}
+		#endregion
+	}
+}
