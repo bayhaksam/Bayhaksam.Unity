743ed3a5a712a66bde37efd88a1654f8d90eb0e3
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2019-03-15 09:55:23 +0300

    Added certificate handler which accepts all certificates

---
+++ src/src/Net/src/src/certificate/AcceptAllCertificatesHandler.cs
@@ -0,0 +1,16 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AcceptAllCertificatesHandler.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.Net
+{
+	using UnityEngine.Networking;
+
+	public class AcceptAllCertificatesHandler : CertificateHandler
+	{
+		protected override bool ValidateCertificate(byte[] certificateData) => true;
+	}
+}
