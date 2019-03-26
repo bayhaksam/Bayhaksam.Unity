3d05758511fada9b87fa4243d689a7dfa7a50aa6
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2019-03-26 23:51:20 +0300

    Added property drawer for 'UDateTime' custom serializable type

---
+++ src/src/Editor/CustomEditor/src/src/propertyDrawer/UDateTimePropertyDrawer.cs
@@ -0,0 +1,31 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="UDateTimePropertyDrawer.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.Editor.CustomEditor
+{
+	using UnityEditor;
+	using UnityEngine;
+
+	/// <summary>
+	/// Custom <see cref="PropertyDrawer"/> for <see cref="UDateTime"/>.
+	/// </summary>
+	[CustomPropertyDrawer(typeof(UDateTime))]
+	public class UDateTimePropertyDrawer : PropertyDrawer
+	{
+		#region PropertyDrawer Methods
+		/// <inheritdoc/>
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			EditorGUI.BeginProperty(position, label, property);
+			{
+				EditorGUI.PropertyField(position, property.FindPropertyRelative("value"), label);
+			}
+			EditorGUI.EndProperty();
+		}
+		#endregion
+	}
+}
