934895a6aacdac95d65a6397320c3c8ab0f6c007
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2019-06-14 23:49:17 +0300

    [ITextPrinter] Added non-generic text printer abstractions

---
+++ src/src/UI/src/src/Behaviour/textPrinter/TextPrinterBase.cs
@@ -0,0 +1,34 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TextPrinterBase.cs" company="Bayhaksam">
+//      Copyright (c) Bayhaksam. All rights reserved.
+// </copyright>
+// <author>Samet Kurumahmut</author>
+//-----------------------------------------------------------------------
+
+namespace Bayhaksam.Unity.UI.Behaviour
+{
+	using UnityEngine;
+	using UnityEngine.UI;
+
+	public abstract class TextPrinterBase : MonoBehaviour, ITextPrinter
+	{
+		#region Unity Fields
+		[SerializeField]
+		Text text;
+		#endregion
+
+		#region Properties
+		/// <inheritdoc/>
+		public virtual Text Text
+		{
+			get { return this.text; }
+			set { this.text = value; }
+		}
+		#endregion
+
+		#region Public Methods
+		/// <inheritdoc/>
+		public abstract void Execute();
+		#endregion
+	}
+}
